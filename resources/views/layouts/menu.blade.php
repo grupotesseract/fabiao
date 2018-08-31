23d16a89f8e3c134e2dcd6f995fb4520bb13d474
Thiago Braga <thibraga06@gmail.com>
2018-08-31 08:25:47 -0300

    Melhorias na exibição do menu
    Inseri um agrupador no sidebar, para facilitar a visão e evitar textos
    repetidos. Usei o li.header do AdminLTE, que ficou agradável.

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,33 +1,40 @@
+<li class="header">PRINCIPAL</li>
+
 <li class="treeview">
-	<a href="/home"><span>Home</span></a>
+    <a href="/home">Home</a>
 </li>
+
 <li class="treeview">
-	<a href="/users"><span>Usuários</span></a>
+    <a href="/users">Usuários</a>
 </li>
+
+<li class="header">TEXTOS</li>
+
 <li class="{{ Request::is('textoEntradas*') ? 'active' : '' }}">
-    <a href="{!! route('textoEntradas.index') !!}"><span>Texto Entrada</span></a>
+    <a href="{!! route('textoEntradas.index') !!}">Entrada</a>
 </li>
 
 <li class="{{ Request::is('textoExposicaoACrises*') ? 'active' : '' }}">
-    <a href="{!! route('textoExposicaoACrises.index') !!}"><span>Texto Exposição à Crise</span></a>
+    <a href="{!! route('textoExposicaoACrises.index') !!}">Exposição à Crise</a>
 </li>
 
 <li class="{{ Request::is('textoPosicionamentoEstrategicos*') ? 'active' : '' }}">
-    <a href="{!! route('textoPosicionamentoEstrategicos.index') !!}"><span>Texto Posicionamento Estratégico</span></a>
+    <a href="{!! route('textoPosicionamentoEstrategicos.index') !!}">Posicionamento Estratégico</a>
 </li>
 
 <li class="{{ Request::is('textoPosicaoFinanceiras*') ? 'active' : '' }}">
-    <a href="{!! route('textoPosicaoFinanceiras.index') !!}"><span>Texto Posicao Financeira</span></a>
+    <a href="{!! route('textoPosicaoFinanceiras.index') !!}">Posicao Financeira</a>
 </li>
 
 <li class="{{ Request::is('textosCubos*') ? 'active' : '' }}">
-    <a href="{!! route('textosCubos.index') !!}"><span>Textos Cubo</span></a>
+    <a href="{!! route('textosCubos.index') !!}">Cubo</a>
 </li>
 
 <li class="{{ Request::is('textosIniciativas*') ? 'active' : '' }}">
-    <a href="{!! route('textosIniciativas.index') !!}"><span>Textos Iniciativa</span></a>
+    <a href="{!! route('textosIniciativas.index') !!}">Iniciativa</a>
 </li>
+
 <li class="{{ Request::is('textosDetalhamentoIniciativas*') ? 'active' : '' }}">
-    <a href="{!! route('textosDetalhamentoIniciativas.index') !!}"><span>Textos Detalhamento Iniciativa</span></a>
+    <a href="{!! route('textosDetalhamentoIniciativas.index') !!}">Detalhamento Iniciativa</a>
 </li>
 
