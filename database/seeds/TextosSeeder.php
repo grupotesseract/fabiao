163c4442c419054b6e6792b11c593479a676946b
Lucas Gouvêa (K2) <lucas.fim@gmail.com>
2018-09-19 11:28:17 -0300

    feature/seeder_textos | Acertando formatação zuada no seeder e atualizando valores de compra

--- database/seeds/TextosSeeder.php
+++ database/seeds/TextosSeeder.php
@@ -18,96 +18,96 @@ class TextosSeeder extends Seeder
      */
     public function run()
     {
-		TextoEntrada::create([
-			'cabecalho' => 'Descubra através de uma rapida análise, em apenas 3 perguntas, qual o posicionamento estratégico da sua empresa e o momento em que ela se encontra',
-			'rodape' => 'Após este estudo voce poderá conhecer as ações e iniciativas específicas e recomendadas para cada ponto estratégico.',
-		]);
+        TextoEntrada::create([
+            'cabecalho' => 'Descubra através de uma rapida análise, em apenas 3 perguntas, qual o posicionamento estratégico da sua empresa e o momento em que ela se encontra',
+            'rodape' => 'Após este estudo voce poderá conhecer as ações e iniciativas específicas e recomendadas para cada ponto estratégico.',
+        ]);
 
-		TextoExposicaoACrise::create([
-			'texto_pergunta' => 'Considerando o Setor de atuação da sua empresa, qual o grau de exposição dela frente a crises?',
-			'texto_resposta_1' => 'Maior',
-			'texto_resposta_2' => 'Menor',
-			'descritivo_resposta_1' => 'A definir',
-			'descritivo_resposta_2' => 'A definir'
-		]);
+        TextoExposicaoACrise::create([
+            'texto_pergunta' => 'Considerando o Setor de atuação da sua empresa, qual o grau de exposição dela frente a crises?',
+            'texto_resposta_1' => 'Maior',
+            'texto_resposta_2' => 'Menor',
+            'descritivo_resposta_1' => 'A definir',
+            'descritivo_resposta_2' => 'A definir'
+        ]);
 
-		TextoPosicaoFinanceira::create([
-			'texto_pergunta' => 'Qual a situação financeira da sua Empresa?',
-			'texto_resposta_1' => 'Forte',
-			'texto_resposta_2' => 'Fraca',
-			'descritivo_resposta_1' => 'A definir',
-			'descritivo_resposta_2' => 'A definir'
-		]);
+        TextoPosicaoFinanceira::create([
+            'texto_pergunta' => 'Qual a situação financeira da sua Empresa?',
+            'texto_resposta_1' => 'Forte',
+            'texto_resposta_2' => 'Fraca',
+            'descritivo_resposta_1' => 'A definir',
+            'descritivo_resposta_2' => 'A definir'
+        ]);
 
-		TextoPosicionamentoEstrategico::create([
-			'texto_pergunta' => 'Como você considera o posicionamento estratégico da sua empresa no mercado ?',
-			'texto_resposta_1' => 'Forte',
-			'texto_resposta_2' => 'Fraco',
-			'descritivo_resposta_1' => 'A definir',
-			'descritivo_resposta_2' => 'A definir'
-		]);
+        TextoPosicionamentoEstrategico::create([
+            'texto_pergunta' => 'Como você considera o posicionamento estratégico da sua empresa no mercado ?',
+            'texto_resposta_1' => 'Forte',
+            'texto_resposta_2' => 'Fraco',
+            'descritivo_resposta_1' => 'A definir',
+            'descritivo_resposta_2' => 'A definir'
+        ]);
 
-		TextosCubo::create([
-			[
-				'descritivo' => 'Reabastecer o tanque',
-				'resposta_ec' => 'Maior',
-				'resposta_pe' => 'Forte',
-				'resposta_pf' => 'Fraca',
-				'valor_compra' => '50'
-			],
-			[
-				'descritivo' => 'S.O.S',
-				'resposta_ec' => 'Maior',
-				'resposta_pe' => 'Fraco',
-				'resposta_pf' => 'Fraca',
-				'valor_compra' => '50'
-			],
-			[
-				'descritivo' => 'Ampliar liderança',
-				'resposta_ec' => 'Maior',
-				'resposta_pe' => 'Forte',
-				'resposta_pf' => 'Forte',
-				'valor_compra' => '50'
-			],
-			[
-				'descritivo' => 'Atravessar com cuidado',
-				'resposta_ec' => 'Maior',
-				'resposta_pe' => 'Fraco',
-				'resposta_pf' => 'Forte',
-				'valor_compra' => '50'
-			],
-			[
-				'descritivo' => 'Livrar-se do peso extra',
-				'resposta_ec' => 'Menor',
-				'resposta_pe' => 'Forte',
-				'resposta_pf' => 'Fraca',
-				'valor_compra' => '50'
-			],
-			[
-				'descritivo' => 'Reencontrar o foco',
-				'resposta_ec' => 'Menor',
-				'resposta_pe' => 'Fraco',
-				'resposta_pf' => 'Fraca',
-				'valor_compra' => '50'
-			],
-			[
-				'descritivo' => 'Seguir em velocidade máxima',
-				'resposta_ec' => 'Menor',
-				'resposta_pe' => 'Forte',
-				'resposta_pf' => 'Forte',
-				'valor_compra' => '50'
-			],
-			[
-				'descritivo' => 'Ultrapassar os tímidos',
-				'resposta_ec' => 'Menor',
-				'resposta_pe' => 'Fraco',
-				'resposta_pf' => 'Forte',
-				'valor_compra' => '50'
-			],
-		]);
+        TextosCubo::create([
+            [
+                'descritivo' => 'Reabastecer o tanque',
+                'resposta_ec' => 'Maior',
+                'resposta_pe' => 'Forte',
+                'resposta_pf' => 'Fraca',
+                'valor_compra' => '9'
+            ],
+            [
+                'descritivo' => 'S.O.S',
+                'resposta_ec' => 'Maior',
+                'resposta_pe' => 'Fraco',
+                'resposta_pf' => 'Fraca',
+                'valor_compra' => '9'
+            ],
+            [
+                'descritivo' => 'Ampliar liderança',
+                'resposta_ec' => 'Maior',
+                'resposta_pe' => 'Forte',
+                'resposta_pf' => 'Forte',
+                'valor_compra' => '9'
+            ],
+            [
+                'descritivo' => 'Atravessar com cuidado',
+                'resposta_ec' => 'Maior',
+                'resposta_pe' => 'Fraco',
+                'resposta_pf' => 'Forte',
+                'valor_compra' => '9'
+            ],
+            [
+                'descritivo' => 'Livrar-se do peso extra',
+                'resposta_ec' => 'Menor',
+                'resposta_pe' => 'Forte',
+                'resposta_pf' => 'Fraca',
+                'valor_compra' => '9'
+            ],
+            [
+                'descritivo' => 'Reencontrar o foco',
+                'resposta_ec' => 'Menor',
+                'resposta_pe' => 'Fraco',
+                'resposta_pf' => 'Fraca',
+                'valor_compra' => '9'
+            ],
+            [
+                'descritivo' => 'Seguir em velocidade máxima',
+                'resposta_ec' => 'Menor',
+                'resposta_pe' => 'Forte',
+                'resposta_pf' => 'Forte',
+                'valor_compra' => '9'
+            ],
+            [
+                'descritivo' => 'Ultrapassar os tímidos',
+                'resposta_ec' => 'Menor',
+                'resposta_pe' => 'Fraco',
+                'resposta_pf' => 'Forte',
+                'valor_compra' => '9'
+            ],
+        ]);
 
-		/* TextosIniciativa::create([]); */
+        /* TextosIniciativa::create([]); */
 
-		/* TextosDetalhamentoIniciativa::create([]); */
+        /* TextosDetalhamentoIniciativa::create([]); */
     }
 }
